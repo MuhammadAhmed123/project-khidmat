5bbb1f922e42d3a5c7376f234349e1f0e7d8b96f
Muhammad Ahmed <ma04007@st.habib.edu.pk>
2019-12-30 21:07:18 +0500

    fixed ERD and added comments to app.py

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-engine = create_engine("mysql://root:@localhost/tgsapp")
+engine = create_engine("mysql://root:@127.0.0.1/tgsapplication")
 db = scoped_session(sessionmaker(bind=engine))
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# this registration code has to be edited
+
 @app.route("/registerStudentLink")
 def registerStudentLink():
-    classes = db.execute("SELECT class.Name FROM class")
+    classes = db.execute("SELECT Class.Name FROM Class")
     return render_template("registerStudent.html", classes=classes)
 
 @app.route("/registerStudent", methods=["POST"])
@@ -57,17 +60,19 @@ def registerStaffLink():
 def registerStaff():
     return redirect(url_for('registerStaffLink'))
 
+# below code is running
+
 @app.route("/classLink")
 def classLink():
-    classes = db.execute("SELECT class.Name FROM class")
+    classes = db.execute("SELECT Class.Name FROM Class")
     return render_template("class.html", classes=classes)
 
 @app.route("/editClass", methods=["POST"])
 def editClass():
     existingClass = request.form.get('existingClasses')
-    idExistingClass = (list(db.execute("SELECT idClass FROM class WHERE class.Name = :className",{"className":existingClass})))[0][0]
+    idExistingClass = (list(db.execute("SELECT idClass FROM Class WHERE Class.Name = :className",{"className":existingClass})))[0][0]
     newClass = request.form.get('classEdited')
-    db.execute("UPDATE class SET class.Name = :newclass WHERE class.idClass = :id", {"newclass":newClass, "id":int(idExistingClass)})
+    db.execute("UPDATE Class SET Class.Name = :newclass WHERE Class.idClass = :id", {"newclass":newClass, "id":int(idExistingClass)})
     db.commit()
     return redirect(url_for('classLink'))
 
@@ -75,12 +80,12 @@ def editClass():
 def addClass():
     addClassName = request.form.get('className')
 
-    classes = db.execute("SELECT class.Name FROM class")
+    classes = db.execute("SELECT Class.Name FROM Class")
     for i in classes:
         if i.Name==addClassName:
             return redirect(url_for('classLink'))   #make a separate page for displaying a warning
 
-    db.execute("INSERT INTO class(Name) VALUES (:name)",{"name":addClassName})
+    db.execute("INSERT INTO Class(Name) VALUES (:name)",{"name":addClassName})
     db.commit()
     return redirect(url_for('classLink'))
 
